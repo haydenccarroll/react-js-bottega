c540d2629e46671ba4687423326e6d49a99fd702
Hayden Carroll <haydencarroll77@gmail.com>
2021-06-24 08:13:34 -0700

    finished making the initial api get request

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import axios from "axios";
 import {
     BrowserRouter as Router,
     Switch,
@@ -13,12 +14,32 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
 
+    constructor() {
+        super();
+        this.getPortfolioItems = this.getPortfolioItems.bind(this);
+    }
+    getPortfolioItems() {
+        console.log("Something got printed");
+        const axios = require('axios');
+
+        // Make a request for a user with a given ID
+        axios.get("https://haydenccarroll.devcamp.space/portfolio/portfolio_items")
+        .then(response => {
+            // handle success
+            console.log(response);
+        })
+        .catch( error => {
+            // handle error
+            console.log(error);
+        });
+    }
 
     render() {
-    {console.log("Does this display");}
+        this.getPortfolioItems();
 
         return (
             <div className='app'>
@@ -32,7 +53,8 @@ export default class App extends Component {
                             <Route path="/about-me" component={About} />
                             <Route path="/contact" component={Contact} />
                             <Route path="/blog" component={Blog} />
-                            <Route path="/portfolio/:slug" component={PortfolioDetail} />
+                            <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+                            <Route component={NoMatch} />
                         </Switch>
                     </div>
                 </Router>
